ccdb4a87bf4361ca07254c023acb9509c6941dfb
Mfgarciat <mfgarciat@ufpso.edu.co>
2025-08-13 19:55:56 -0500

    update readme

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import styles from './header.module.css';
+import styles from './Header.module.css';
 import logo from '../../assets/logo-puntored.png';
 import { useAuth } from '../../hooks/useAuth'; 
 import { MdLogout } from "react-icons/md";
